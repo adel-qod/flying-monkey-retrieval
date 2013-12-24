ab1dc948c9194978484fd6d887635a1fde704640
Adel Qodmani <mpcadel@gmail.com>
2013-12-25 01:53:12 +0200

    Moving the location of the DocumentDistance class

--- IR/Program.cs
+++ IR/Program.cs
@@ -8,32 +8,6 @@ using System.IO;
 
 namespace IR
 {
-    /// <summary>
-    /// Represents each document's distance/consinal similarty
-    /// </summary>
-    class DocumentDistance : IComparable<DocumentDistance>
-    {	
-	    public string docName;
-        public string docPath;
-	    public double distance;
-
-	    public DocumentDistance(String docPath)
-	    {
-            this.docPath = docPath;
-            this.docName = Path.GetFileNameWithoutExtension(docPath);
-	    }
-    	
-	    public int CompareTo(DocumentDistance that)
-	    {
-	       	if(this.distance < that.distance)
-	    		return -1;
-		    else if(this.distance == that.distance)
-			    return 0;
-		    else
-			    return 1;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
